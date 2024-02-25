cb3b98d292449aa51461884da23479a786c27a78
Luis Perez <luisperez07182@email.com>
2024-02-24 19:27:57 -0600

    added files and finished prob1

---
+++ spec/prob1_spec.rb
@@ -0,0 +1,20 @@
+require 'prob1'
+RSpec.describe User do
+  describe "#log_in" do
+    it "logs the user in" do
+      u1 = User.new 'awda'
+      u1.log_in
+      expect(u1.logged_in).to be true
+      # Fill in the test
+    end
+  end
+
+  describe "#log_out" do
+    it "logs the user out" do
+      u2 = User.new 'waawdaw'
+      u2.log_out
+      expect(u2.logged_in).to be false
+      # Fill in the test
+    end
+  end
+end
